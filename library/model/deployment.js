c74b9edde5de17f6ac3cbff313e115fd96a9bed9
TanvirSojal <tanvirahmedsojal@gmail.com>
2023-04-05 23:56:40 +0600

    deleted unused type

--- library/model/deployment.js
+++
@@ -1,9 +0,0 @@
-const Resource = require('./resource')
-
-class Deployment extends Resource {
-    constructor(name, apiVersion){
-        super(name, 'Microsoft.Resources/deployments', apiVersion)
-    }
-}
-
-module.exports = Deployment;
